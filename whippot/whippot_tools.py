622808b86e7fd82ef3125da735e21f12f8888f15
Jonathan Aguilar <jaguilar@stsci.edu>
2025-10-22 09:23:22 -0400

    code formatting

--- whippot/whippot_tools.py
+++ whippot/whippot_tools.py
@@ -437,8 +437,12 @@ def compute_offsets(
 
 
     # Offsets to science target
-    sep = star_positions[0]['position'].separation(star_positions[1]['position']).to(units.arcsec)
-    pa = star_positions[0]['position'].position_angle(star_positions[1]['position']).to(units.deg)
+    sep = star_positions[0]['position'].separation(
+        star_positions[1]['position']
+    ).to(units.arcsec)
+    pa = star_positions[0]['position'].position_angle(
+        star_positions[1]['position']
+    ).to(units.deg)
 
     idl_coords = sky_to_idl(star_positions,
                             sci_aper,
@@ -539,111 +543,3 @@ def sky_to_idl(
         pos = star['position']
         idl_coords.append({'label': label, 'position': np.array(aper.sky_to_idl(pos.ra.deg, pos.dec.deg))})
     return idl_coords
-
-
-#--------------------------------------------#
-#----------------- Plotting -----------------#
-#--------------------------------------------#
-# def plot_aper_idl(
-#         aper : pysiaf.aperture.JwstAperture,
-#         star_positions : dict[str, np.ndarray] = {},
-#         offset : list | np.ndarray = [0., 0.],
-#         ax = None,
-#         title = '',
-#         show_legend : bool = True,
-# ):
-#     """Plot the scene on the detector when you're pointed at the science target"""
-#     # plot 1 : POV of the detector
-#     if ax is None:
-#         fig, ax = plt.subplots(1, 1, layout='constrained')
-#     else:
-#         fig = ax.get_figure()
-#     frame = 'idl' # options are: tel (telescope), det (detector), sci (aperture)
-#     if title != '':
-#         ax.set_title(title)
-#     aper.plot(ax=ax, label=False, frame=frame, c='gray', mark_ref=True)
-
-#     offset = np.array(offset)
-
-#     star_positions = star_positions.copy()
-
-#     if 'ACQ' in star_positions.keys():
-#         acq_pos = star_positions.pop('ACQ')
-#         ax.scatter(acq_pos[0] + offset[0], acq_pos[1] + offset[1],
-#                    c='k',
-#                    label=f"ACQ",
-#                    marker='x',
-#                    s=100)
-#     if 'SCI' in star_positions.keys():
-#         sci_pos = star_positions.pop("SCI")
-#         ax.scatter(*(sci_pos+ offset),
-#                    label=f"SCI",
-#                    marker="*",
-#                    c='k')
-#     for star, position in star_positions.items():
-#         ax.scatter(*(position + offset),
-#                    # c='k',
-#                    label=star,
-#                    marker='.',
-#                    s=50)
-#     if show_legend:
-#         ax.legend(loc=(1.05, 0.3))
-#     ax.set_aspect("equal")
-#     ax.grid(True, ls='--', c='grey', alpha=0.5)
-#     return fig
-
-# def plot_aper_sky(
-#         aper : pysiaf.aperture.JwstAperture,
-#         star_positions : dict[str, np.ndarray] = {},
-#         offset : list | np.ndarray = [0., 0.],
-#         ax = None,
-#         title = '',
-#         show_legend : bool = True,
-# ):
-#     """Plot the scene on the detector when you're pointed at the science target"""
-#     # plot 1 : POV of the detector
-#     if ax is None:
-#         fig, ax = plt.subplots(1, 1, layout='constrained')
-#     else:
-#         fig = ax.get_figure()
-#     frame = 'sky' # options are: tel (telescope), det (detector), sci (aperture)
-#     if title != '':
-#         ax.set_title(title)
-#     aper.plot(ax=ax, label=False, frame=frame, c='gray', mark_ref=True)
-#     ax.set_xlabel("RA [deg]")
-#     ax.set_ylabel("Dec [deg]")
-
-#     offset = np.array(offset)
-#     star_positions = star_positions.copy()
-#     if 'ACQ' in star_positions.keys():
-#         acq_pos = star_positions.pop('ACQ')
-#         acq_pos = np.array(acq_pos) + np.array(offset)
-#         acq_pos = aper.idl_to_sky(*acq_pos)
-#         ax.scatter(*acq_pos,
-#                    c='k',
-#                    label=f"ACQ",
-#                    marker='x',
-#                    s=100)
-#     if 'SCI' in star_positions.keys():
-#         sci_pos = star_positions.pop("SCI")
-#         sci_pos = np.array(sci_pos) + np.array(offset)
-#         sci_pos = aper.idl_to_sky(*sci_pos)
-#         ax.scatter(*sci_pos,
-#                    label=f"SCI",
-#                    marker="*",
-#                    c='k')
-#     for star, position in star_positions.items():
-#         position = np.array(position) + np.array(offset)
-#         position = aper.idl_to_sky(*position)
-#         ax.scatter(*position, 
-#                    # c='k',
-#                    label=star,
-#                    marker='.',
-#                    s=50)
-#     if show_legend:
-#         ax.legend(loc=(1.05, 0.3))
-#     ax.set_aspect("auto")
-#     ax.grid(True, ls='--', c='grey', alpha=0.5)
-#     ax.invert_xaxis()
-#     return fig
-
