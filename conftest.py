95bedd242a166a09da1746ab9328ca548f676715
Jonathan Aguilar <jaguilar@stsci.edu>
2025-10-28 12:26:21 -0400

    added some very rudimentary tests

--- conftest.py
+++ conftest.py
@@ -6,16 +6,16 @@ except ImportError:
     TESTED_VERSIONS = {}
 
 try:
-    from packagename import __version__ as version
+    from whippot import __version__ as version
 except ImportError:
     version = 'unknown'
 
 # Uncomment and customize the following lines to add/remove entries
 # from the list of packages for which version numbers are displayed
 # when running the tests.
-PYTEST_HEADER_MODULES['astropy'] = 'astropy'
-PYTEST_HEADER_MODULES.pop('Matplotlib')
-PYTEST_HEADER_MODULES.pop('Pandas')
-PYTEST_HEADER_MODULES.pop('h5py')
+# PYTEST_HEADER_MODULES['astropy'] = 'astropy'
+# PYTEST_HEADER_MODULES.pop('Matplotlib')
+# PYTEST_HEADER_MODULES.pop('Pandas')
+# PYTEST_HEADER_MODULES.pop('h5py')
 
-TESTED_VERSIONS['packagename'] = version
+# TESTED_VERSIONS['packagename'] = version
---
+++ whippot/tests/test_computepositions.py
@@ -0,0 +1,39 @@
+import pytest
+
+from whippot import whippot_tools
+from whippot.modes import miri_lrs_slitless_tools
+from whippot.modes import miri_wfss_tools
+
+sources = {
+    'SCI': whippot_tools.SkyCoord("05 24 20.7552 -70 05 1.60", frame='icrs', unit=("hourangle","degree")),
+    'a': whippot_tools.SkyCoord("05 24 26.33969 -70 05 22.3545", frame='icrs', unit=("hourangle","degree")),
+    'b': whippot_tools.SkyCoord("05 24 28.67861 -70 05 24.4484", frame='icrs', unit=("hourangle","degree")),
+    'c': whippot_tools.SkyCoord("05 24 36.22460 -70 05 28.1876", frame='icrs', unit=("hourangle","degree")),
+    'd': whippot_tools.SkyCoord("05 24 25.608 -70 05 01.66", frame='icrs', unit=("hourangle","degree")),
+}
+
+default_init = {
+    'instr': 'miri',
+    'sci_aper': 'mirim_illum', 
+    'pa': 290.,
+    'sci_ra': sources['SCI'].ra.deg, 'sci_dec': sources['SCI'].dec.deg,
+}
+# add a multi-line string of the other stars, copied from the cell above
+default_init['other_stars'] = "\n".join(f"{k}: ({v.ra.deg}, {v.dec.deg})" for k, v in sources.items())
+
+# test that things run without crashing
+def test_ComputePositions():
+    cp = whippot_tools.ComputePositions(initial_values=default_init)
+    fig = cp.plot_scene()
+    whippot_tools.plt.close(fig)
+
+def test_MiriLrsSlitless_ComputePositions():
+    cp = miri_lrs_slitless_tools.ComputePositions(initial_values=default_init)
+    fig = cp.plot_scene()
+    whippot_tools.plt.close(fig)
+
+def test_MiriWFSS_ComputePositions():
+    cp = miri_wfss_tools.ComputePositions(initial_values=default_init)
+    fig = cp.plot_scene()
+    whippot_tools.plt.close(fig)
+
