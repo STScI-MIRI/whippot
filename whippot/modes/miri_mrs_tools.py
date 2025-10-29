056803602fb2fddaefc394c06bdbdcb3249f3a68
Jonathan Aguilar <jaguilar@stsci.edu>
2025-10-29 16:08:22 -0400

    MRS default plot now shows channel and channel/slice footprints

--- whippot/modes/miri_mrs_tools.py
+++ whippot/modes/miri_mrs_tools.py
@@ -30,44 +30,25 @@ class ComputePositions(whippot_tools.ComputePositions):
         super().plot_scene.__doc__
 
         fig = super().plot_scene(self, *args)
+        # get the IDL and Sky axes
         idl_ax, sky_ax = fig.get_axes()
-        # idl_axes, sky_axes = fig.get_axes()
 
-        # for the chosen channel, show the slits
+        # for the chosen channel, overlay the slit footprints on the axes
         channelid = self.aperture.AperName[-2:]
         slit_apernames = [i for i in self.instr.apernames if f'MIRIFU_{channelid}' in i]
-        print(channelid, slit_apernames)
         for apername in slit_apernames:
             new_aper = self.instr[apername]
-            # footprint = whippot_tools.transform_aper_footprint(new_aper, self.aperture, 'idl', label=apername)
-            # idl_ax.add_patch(footprint)
-            # footprint = whippot_tools.transform_aper_footprint(new_aper, self.aperture, 'sky', label=apername)
-            # sky_ax.add_patch(footprint)
+            # idl
+            footprint = whippot_tools.transform_aper_footprint(
+                new_aper, self.aperture, 'idl',
+                label=apername, zorder=-1
+            )
+            idl_ax.add_patch(footprint)
+            # sky
+            footprint = whippot_tools.transform_aper_footprint(
+                new_aper, self.aperture, 'sky',
+                label=apername, zorder=-1
+            )
+            sky_ax.add_patch(footprint)
 
-    #     trace_properties = dict(
-    #         facecolor='C0', alpha=0.5, zorder=-1, linestyle='none'
-    #     )
-
-    #     # for each source, add its trace to the detector and sky axes
-    #     idl_traces, sky_traces = [], []
-    #     for i, (k, coord) in enumerate(self.idl_coords_after_slew.items()):
-    #         # plot each trace as a Rectangle, defining the height, width, and bottom corner
-    #         height, width = (TRACE_UP + TRACE_DOWN), 1
-    #         # ll -> lower left corner
-    #         ll = (coord[0]-width/2, coord[1]-TRACE_DOWN)
-    #         idl_traces.append(mpl.patches.Rectangle(ll, width, height, **trace_properties))
-
-    #         # transform the trace idl vertices to sky coordinates
-    #         sky_traces.append(whippot_tools.transform_patch_footprint(
-    #             idl_traces[-1], self.aperture, 'idl', 'sky', **trace_properties
-    #         ))
-    #     # for some reason you have to find the axis limits *before* you add the patches to the plot,
-    #     # perhaps because the act of adding them changes their vertices
-    #     whippot_plots.include_patches_in_axes(idl_ax, idl_traces, invert_ra_axis=False)
-    #     whippot_plots.include_patches_in_axes(sky_ax, sky_traces, invert_ra_axis=True)
-    #     # add the sky trace to the sky axes
-    #     # add the idl trace to the idl axis
-    #     for it, st in zip(idl_traces, sky_traces):
-    #         idl_ax.add_patch(it)
-    #         sky_ax.add_patch(st)
-    #     return fig
+        return fig
