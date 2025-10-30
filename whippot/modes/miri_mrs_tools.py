971f6dcfb7bc3d7ae2c647f50db763efc99408c5
Jonathan Aguilar <jaguilar@stsci.edu>
2025-10-30 14:49:19 -0400

    improved MRS plots to show channels and slices
    MRS plots now show all 4 channels, and the individual slices for the chosen
    channel. This is true also if the user selects an individual slice in the
    Aperture selection menu.

--- whippot/modes/miri_mrs_tools.py
+++ whippot/modes/miri_mrs_tools.py
@@ -1,6 +1,8 @@
 """
 Miscellaneous plot methods
 """
+import re
+
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 import numpy as np
@@ -17,13 +19,13 @@ TRACE_DOWN = 300 * aper.YSciScale
 # with the one defined above
 class ComputePositions(whippot_tools.ComputePositions):
 
-    def aperture_filter(self):
-        """
-        Function that returns a filtered list of apertures that can be selected
-        Written this way so that it can be overriden by subclasses
-        """
-        apernames = [i for i in  whippot_tools.Siaf(self.parameter_values['instr']).apernames if 'MIRIFU_CHANNEL' in i]
-        return list(apernames)
+    # def filter_aperture_options(self):
+    #     """
+    #     Function that returns a filtered list of apertures that can be selected
+    #     Written this way so that it can be overriden by subclasses
+    #     """
+    #     apernames = [i for i in  whippot_tools.Siaf(self.parameter_values['instr']).apernames if 'MIRIFU_CHANNEL' in i]
+    #     return list(apernames)
 
     def plot_scene(self, *args) -> mpl.figure.Figure:
         # copy the docstring
@@ -33,22 +35,54 @@ class ComputePositions(whippot_tools.ComputePositions):
         # get the IDL and Sky axes
         idl_ax, sky_ax = fig.get_axes()
 
-        # for the chosen channel, overlay the slit footprints on the axes
-        channelid = self.aperture.AperName[-2:]
-        slit_apernames = [i for i in self.instr.apernames if f'MIRIFU_{channelid}' in i]
+        channelid, band = re.search("[1-4][A-C]", self.aperture.AperName).group()
+
+        # overlay the channel footprints as well as the slits for the selected channel
+
+        channel_apernames = [f'MIRIFU_CHANNEL{i}{band}' for i in [1, 2, 3, 4]]
+        slit_apernames = [i for i in self.instr.apernames if f'MIRIFU_{channelid}{band}' in i]
+
+        idl_patches, sky_patches = [], []
         for apername in slit_apernames:
+            slit_params = dict(label=apername, zorder=-1, alpha=0.3, ec='k')
             new_aper = self.instr[apername]
             # idl
             footprint = whippot_tools.transform_aper_footprint(
                 new_aper, self.aperture, 'idl',
-                label=apername, zorder=-1
+                **slit_params,
             )
-            idl_ax.add_patch(footprint)
+            idl_patches.append(footprint)
             # sky
             footprint = whippot_tools.transform_aper_footprint(
                 new_aper, self.aperture, 'sky',
-                label=apername, zorder=-1
+                **slit_params,
             )
-            sky_ax.add_patch(footprint)
+            sky_patches.append(footprint)
+
+        for apername in channel_apernames:
+            channel_params = dict(label=apername, zorder=-1, alpha=0.2, ec='k', lw=2)
+            new_aper = self.instr[apername]
+            # idl
+            footprint = whippot_tools.transform_aper_footprint(
+                new_aper, self.aperture, 'idl',
+                **channel_params,
+            )
+            idl_patches.append(footprint)
+            # sky
+            footprint = whippot_tools.transform_aper_footprint(
+                new_aper, self.aperture, 'sky',
+                **channel_params,
+            )
+            sky_patches.append(footprint)
+
+        # adjust the axes
+        whippot_plots.include_patches_in_axes(idl_ax, idl_patches)
+        whippot_plots.include_patches_in_axes(sky_ax, sky_patches)
+        # add the footprints to the axes
+        for patch in idl_patches:
+            idl_ax.add_patch(patch)
+        for patch in sky_patches:
+            sky_ax.add_patch(patch)
+
 
         return fig
