341dfc902c3c68389de166bc5c12197136cd2a1d
Jonathan Aguilar <jaguilar@stsci.edu>
2025-11-12 13:20:06 -0500

    MIRI_WFSS now has a function to overplot traces on data

--- whippot/modes/miri_wfss_tools.py
+++ whippot/modes/miri_wfss_tools.py
@@ -10,6 +10,10 @@ from whippot import whippot_tools, whippot_plots
 aper = whippot_tools.Siaf("MIRI")['MIRIM_ILLUM']
 TRACE_UP = 100 * aper.YSciScale
 TRACE_DOWN = 300 * aper.YSciScale
+# shared plotting properties for traces
+trace_properties = dict(
+    facecolor='C0', alpha=0.5, zorder=-1, linestyle='none'
+)
 
 # Make a new class that overrides whippot_tools.ComputePositions.plot_scene()
 # with the one defined above
@@ -34,9 +38,6 @@ class ComputePositions(whippot_tools.ComputePositions):
 
         # for each source, add its trace to the detector and sky axes
         idl_traces, sky_traces = [], []
-        trace_properties = dict(
-            facecolor='C0', alpha=0.5, zorder=-1, linestyle='none'
-        )
         for i, (k, coord) in enumerate(self.idl_coords_after_slew.items()):
             # plot each trace as a Rectangle, defining the height, width, and bottom corner
             height, width = (TRACE_UP + TRACE_DOWN), 1
@@ -59,3 +60,72 @@ class ComputePositions(whippot_tools.ComputePositions):
             sky_ax.add_patch(st)
 
         return fig
+
+def plot_on_data(
+        cp : ComputePositions,
+        img : np.ndarray,
+        hdr : dict | None = None,
+) -> mpl.figure.Figure:
+    """
+    Overlay the scene on provided data. You have to be careful here about handling the aperutre name
+
+    Parameters
+    ----------
+    cp : ComputePositions
+    img : np.ndarray
+    hdr : dict : None
+
+    Output
+    ------
+    fig : mpl.figure.Figure hande
+
+    """
+    # get an aperture defined on the detector
+    idl_pos = cp.idl_coords_after_slew
+    # if your aperture can go straight between idl and sci, great
+    # if not, go through tel
+    aper_prefix = cp.aperture.AperName.split("_")[0]
+    subarray_aper = whippot_tools.Siaf(hdr['INSTRUME'])[aper_prefix + "_" + hdr['SUBARRAY']]
+    try:
+        det_pos = {k: cp.aperture.idl_to_sci(*v) for k, v in idl_pos.items()}
+    except TypeError:
+        tel_pos = {k: cp.aperture.idl_to_tel(*v) for k, v in idl_pos.items()}
+        det_pos = {k: subarray_aper.tel_to_sci(*v) for k, v in tel_pos.items()}
+    dims = img.shape
+    xcoords = np.arange(dims[1]+1) + 0.5
+    ycoords = np.arange(dims[0]+1) + 0.5
+    fig, ax = plt.subplots(1, 1)
+    vmin, vmax = np.nanquantile(img, [0.01, 0.99])
+    ax.pcolormesh(
+        xcoords, ycoords, img,
+        vmin=vmin, vmax=vmax,
+        cmap=mpl.cm.cividis,
+    )
+    ax.set_aspect("equal")
+    for label, pos in det_pos.items():
+        ax.scatter(*pos, label=label, marker='x', s=100)
+    ax.legend(loc=(1.1, 0.5))
+
+    traces = []
+    properties = trace_properties.copy()
+    properties.update({'zorder': 10})
+    for i, (k, coord) in enumerate(det_pos.items()):
+        # plot each trace as a Rectangle, defining the height, width, and bottom corner
+        height, width = (TRACE_UP + TRACE_DOWN) / subarray_aper.YSciScale, 1 / subarray_aper.YSciScale
+        # ll -> lower left corner
+        ll = (coord[0]-width/2, coord[1]-TRACE_DOWN/subarray_aper.YSciScale)
+        idl_trace = mpl.patches.Rectangle(ll, width, height, **properties)
+        traces.append(idl_trace)
+        # transform the trace idl vertices to sky coordinates
+        # traces.append(whippot_plots.transform_patch_footprint(
+        #     idl_trace, subarray_aper, 'sci', 'sci', **trace_properties
+        # ))
+    # for some reason you have to find the axis limits *before* you add the patches to the plot,
+    # perhaps because the act of adding them changes their vertices
+    whippot_plots.include_patches_in_axes(ax, traces, invert_ra_axis=False)
+    for t in traces:
+        ax.add_patch(t)
+
+    return fig
+
+
